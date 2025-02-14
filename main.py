0e61e86644176e47b46ac93e3d824d5ab9b3ab8c
9c <ericgwhuang@gmail.com>
2025-02-14 16:30:55 +0800

    feat: Added BaseModel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from pymongo import MongoClient
 from config import config
 """
@@ -14,6 +15,14 @@ db = client["tricking_db"]
 users = db["users"]
 app = FastAPI()
 
+class UserCreate(BaseModel):
+    name: str
+    password: str
+    email: str
+    phone: str
+    sex: str|bool
+    age: int
+
 
 @app.get("/count")
 async def count():
@@ -27,4 +36,19 @@ async def delete_all():
 @app.post("/add-one")
 async def add_one():
     users.insert_one({"username": "aitnyaoytwufwyuw"})
+    return {"status_code": True}
+
+# 創建帳號 API
+@app.post("/api/create-account")
+async def create_account(user: UserCreate):
+    if users.find_one({"name": user.name}):
+        raise HTTPException(status_code=400, detail="使用者名稱已存在")
+    if type(user.sex) != bool:
+        if user.sex.lower() == "male":
+            user.sex = False
+        elif user.sex.lower() == "female":
+            user.sex = True
+        else:
+            raise HTTPException(status_code=401, detail="不合理性別")
+    users.insert_one(user.model_dump())
     return {"status_code": True}
